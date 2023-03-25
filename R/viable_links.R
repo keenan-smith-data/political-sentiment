5d464922354a8730a7ad00dcb4bd46eb149a1a94
keenan-smith-data <74223606+keenan-smith-data@users.noreply.github.com>
2023-03-25 00:15:10 -0400

    scraping functions start

--- R/viable_links.R
+++ R/viable_links.R
@@ -109,25 +109,140 @@ comf_include <- c("blog", "publications", "feed")
 epi_include <- c("blog")
 osf_include <- c("voices", "publications")
 tnat_include <- c("article")
+
 # Viable Links
-filtered_aei <- sitemap_viable_links(sitemaps, short.source = "aei", url.filter = aei_include)
-filtered_cato <- sitemap_viable_links(sitemaps, short.source = "cato", url.filter = cato_include)
-filtered_hrw <- sitemap_viable_links(sitemaps, short.source = "hrw", url.filter = hrw_include)
-filtered_heritage <- sitemap_viable_links(sitemaps, short.source = "heritage", url.filter = heritage_include)
-filtered_cap <- sitemap_viable_links(sitemaps, short.source = "cap", url.filter = cap_include)
-filtered_urban <- sitemap_viable_links(sitemaps, short.source = "urban", url.filter = urban_include)
-filtered_merc <- sitemap_viable_links(sitemaps, short.source = "merc", url.filter = merc_include)
-filtered_mani <- sitemap_viable_links(sitemaps, short.source = "mani", url.filter = mani_include)
-filtered_cbpp <- sitemap_viable_links(sitemaps, short.source = "cbpp", url.filter = cbpp_include)
-filtered_am <- sitemap_viable_links(sitemaps, short.source = "am", url.filter = am_include)
-filtered_disc <- sitemap_viable_links(sitemaps, short.source = "disc", url.filter = disc_include)
+filtered_aei <- sitemap_viable_links(sitemaps, short.source = "aei", url.filter = aei_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), aei_include[1], stringr::regex("/\\w"))) ~ aei_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), aei_include[2], stringr::regex("/\\w"))) ~ aei_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), aei_include[3], stringr::regex("/\\w"))) ~ aei_include[3]
+    ))
+
+filtered_cato <- sitemap_viable_links(sitemaps, short.source = "cato", url.filter = cato_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cato_include[1], stringr::regex("/\\w"))) ~ cato_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cato_include[2], stringr::regex("/\\w"))) ~ heritage_include[2]
+  ))
+
+filtered_hrw <- sitemap_viable_links(sitemaps, short.source = "hrw", url.filter = hrw_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), hrw_include[1], stringr::regex("/\\w"))) ~ hrw_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), hrw_include[2], stringr::regex("/\\w"))) ~ hrw_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), hrw_include[3], stringr::regex("/\\w"))) ~ hrw_include[3]
+  ))
+
+filtered_heritage <- sitemap_viable_links(sitemaps, short.source = "heritage", url.filter = heritage_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), heritage_include[1], stringr::regex("/\\w"))) ~ heritage_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), heritage_include[2], stringr::regex("/\\w"))) ~ heritage_include[2]
+  ))
+
+filtered_cap <- sitemap_viable_links(sitemaps, short.source = "cap", url.filter = cap_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cap_include[1], stringr::regex("/\\w"))) ~ cap_include[1]
+  ))
+
+filtered_urban <- sitemap_viable_links(sitemaps, short.source = "urban", url.filter = urban_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), urban_include[1], stringr::regex("/\\w"))) ~ urban_include[1]
+  ))
+
+filtered_merc <- sitemap_viable_links(sitemaps, short.source = "merc", url.filter = merc_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), merc_include[1], stringr::regex("/\\w"))) ~ merc_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), merc_include[2], stringr::regex("/\\w"))) ~ merc_include[2]
+  ))
+
+filtered_mani <- sitemap_viable_links(sitemaps, short.source = "mani", url.filter = mani_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), mani_include[1], stringr::regex("/\\w"))) ~ mani_include[1]
+  ))
+
+filtered_cbpp <- sitemap_viable_links(sitemaps, short.source = "cbpp", url.filter = cbpp_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cbpp_include[1], stringr::regex("/\\w"))) ~ cbpp_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cbpp_include[2], stringr::regex("/\\w"))) ~ cbpp_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), cbpp_include[3], stringr::regex("/\\w"))) ~ cbpp_include[3]
+  ))
+
+filtered_am <- sitemap_viable_links(sitemaps, short.source = "am", url.filter = am_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), am_include[1], stringr::regex("/\\w"))) ~ am_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), am_include[2], stringr::regex("/\\w"))) ~ am_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), am_include[3], stringr::regex("/\\w"))) ~ am_include[3],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), am_include[4], stringr::regex("/\\w"))) ~ am_include[4]
+  ))
+
+filtered_disc <- sitemap_viable_links(sitemaps, short.source = "disc", url.filter = disc_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), disc_include[1], stringr::regex("/\\w"))) ~ disc_include[1]
+  ))
+
 filtered_epic <- sitemap_viable_links(sitemaps, short.source = "epic", url.filter = epic_exclude, exclude = TRUE)
-filtered_gutt <- sitemap_viable_links(sitemaps, short.source = "gutt", url.filter = gutt_include)
+
+filtered_gutt <- sitemap_viable_links(sitemaps, short.source = "gutt", url.filter = gutt_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), gutt_include[1], stringr::regex("/\\w"))) ~ gutt_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), gutt_include[2], stringr::regex("/\\w"))) ~ gutt_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), gutt_include[3], stringr::regex("/\\w"))) ~ gutt_include[3],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), gutt_include[4], stringr::regex("/\\w"))) ~ gutt_include[4]
+  ))
+
 filtered_comf <- sitemap_viable_links(linkchecker, short.source = "comf", url.filter = comf_include) |>
-  filter(size > 1)
+  filter(size > 1) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), comf_include[1], stringr::regex("/\\w"))) ~ comf_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), comf_include[2], stringr::regex("/\\w"))) ~ comf_include[2],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), comf_include[3], stringr::regex("/\\w"))) ~ comf_include[3]
+  ))
+
 filtered_epi <- sitemap_viable_links(linkchecker, short.source = "epi", url.filter = epi_include) |>
-  filter(size > 1)
+  filter(size > 1) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), epi_include[1], stringr::regex("/\\w"))) ~ epi_include[1]
+  ))
+
 filtered_osf <- sitemap_viable_links(linkchecker, short.source = "osf", url.filter = osf_include) |>
-  filter(size > 1)
+  filter(size > 1) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), osf_include[1], stringr::regex("/\\w"))) ~ osf_include[1],
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), osf_include[2], stringr::regex("/\\w"))) ~ osf_include[2]
+  ))
+
+filtered_tnat <- sitemap_viable_links(linkchecker, short.source = "tnat", url.filter = tnat_include) |>
+  mutate(url_type = case_when(
+    stringr::str_detect(url, paste0(stringr::regex("\\w/"), tnat_include[1], stringr::regex("/\\w"))) ~ tnat_include[1],
+  ))
+
 # Disconnecting from DuckDB
-dbDisconnect(pol_sent_db)
+dbDisconnect(pol_sent_db, shutdown = TRUE)
+
+rm(
+  pol_sent_db,
+  sitemaps,
+  linkchecker,
+  source_table,
+  sitemap_viable_links,
+  initial_helper,
+  initial_look,
+  path_examination,
+  aei_include,
+  cato_include,
+  hrw_include,
+  heritage_include,
+  cap_include,
+  urban_include,
+  merc_include,
+  mani_include,
+  cbpp_include,
+  am_include,
+  disc_include,
+  epic_exclude,
+  gutt_include,
+  comf_include,
+  epi_include,
+  osf_include,
+  tnat_include
+)
+
+detach(package:duckdb, unload = TRUE)
